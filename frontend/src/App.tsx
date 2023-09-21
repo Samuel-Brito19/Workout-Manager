acf7fee474f406e54c684888614d90c9df13ea19
Samuel Brito <sambriton7@gmail.com>
2023-09-21 20:05:37 -0300

    Adding dependencies in the frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,21 @@
 function App() {
 
   return (
-    <h1>Hello</h1>
+    <div>
+      <form>
+        <h1>LOGIN</h1>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" placeholder="Enter Email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" placeholder="Enter Password" />
+        </div>
+        <button>Login</button>
+      </form>
+
+    </div>
   )
 }
 
