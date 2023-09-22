c6c40c3a9e0f3829b3de2c82f87d5bc973d4e870
Samuel Brito <sambriton7@gmail.com>
2023-09-22 11:07:36 -0300

    Finishing the style in the frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,39 @@
-
+import * as Styled from './styles/style'
+import { useState } from 'react'
 
 function App() {
-
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   return (
-    <div>
-      <form>
-        <h1>LOGIN</h1>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="email" placeholder="Enter Email" />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input type="password" placeholder="Enter Password" />
-        </div>
-        <button>Login</button>
-      </form>
 
-    </div>
+    <Styled.Container>
+      <div>
+
+        <Styled.LoginWrap>
+          <form>
+            <h2>LOGIN</h2>
+            <div>
+
+              <input type="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                placeholder="Enter Email" />
+            </div>
+            <div>
+
+              <input type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                placeholder="Enter Password" />
+            </div>
+            <button>Login</button>
+          </form>
+        </Styled.LoginWrap>
+
+
+      </div>
+    </Styled.Container >
+
   )
 }
 
---
+++ frontend/src/styles/style.ts
@@ -0,0 +1,55 @@
+import styled from 'styled-components'
+
+
+export const Container = styled.div`
+height: 100vh;
+display: flex;
+flex-wrap: wrap;
+align-items: center;
+justify-content: center;
+background-color: #008080;
+
+& h2 {
+    font-size: 30px;
+    color: teal;
+    margin-bottom: 20px;
+}
+`
+
+export const LoginWrap = styled.form`
+
+width: 300px;
+background-color: #b9e7e7;
+border-radius: 10px;
+overflow: hidden;
+height: 400px;
+
+& form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+    padding: 50px;
+}
+
+& input {
+    padding: 10px;
+    border: none;
+    border-bottom: 1px solid gray;
+    width: 250px;
+
+}
+
+& button {
+    cursor: pointer;
+    padding: 10px;
+    background-color: teal;
+    color: white;
+    border: none;
+    width: 150px;
+    height: 40px;
+
+}
+
+`
