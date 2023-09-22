16465dddd8c1de379148cec3f727c452a7670eed
Samuel Brito <sambriton7@gmail.com>
2023-09-22 20:00:54 -0300

    Creating a authenticator controller

--- api/src/controller/AuthController.ts
+++ api/src/controller/AuthController.ts
@@ -1,18 +1,29 @@
 import { compare, hash } from "bcryptjs";
 import { prisma } from "../utils/prisma";
 import { Request, Response } from "express";
+import { sign } from "jsonwebtoken";
 
 
-export class UserController {
+export class AuthController {
 
-    async store(req: Request, res: Response) {
+    async authenticate(req: Request, res: Response) {
         const { email, password } = req.body
 
         const user = await prisma.user.findUnique({ where: { email } })
 
-        const isValuePassword = await compare()
+        if (!user) {
+            return res.json({ erro: "User not found!" })
+        }
+
+        const isValuePassword = await compare(password, user.password)
+
+        if (!isValuePassword) {
+            return res.json({ erro: "Invalid password" })
+        }
+
+        const token = sign({ id: user.id }, "secret", { expiresIn: "id" })
 
 
-        return res.json({ user })
+        return res.json({ user, token })
     }
 }
--- api/src/controller/UserController.ts
+++ api/src/controller/UserController.ts
@@ -6,15 +6,24 @@ import { Request, Response } from "express";
 export class UserController {
     async index(req: Request, res: Response) {
         const users = await prisma.user.findMany()
+
         return res.json({ users })
     }
     async store(req: Request, res: Response) {
         const { name, email, password } = req.body
+
+        const userExists = await prisma.user.findUnique({ where: { email: 'samuel@bitcoin.com' } })
+
+        if (!userExists) {
+            return res.json({ error: "User not found!" })
+        }
+
         const hash_password = await hash(password, 8)
+
         const user = await prisma.user.create({
             data: {
-                name,
-                email,
+                name: 'Samukowski',
+                email: 'samuel@bitcoin.com',
                 password: hash_password
             }
         })
