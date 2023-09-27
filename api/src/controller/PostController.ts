5ebbf99677217b3c809d6d695255446489dde1be
Samuel Brito <sambriton7@gmail.com>
2023-09-27 09:31:38 -0300

    Creating a add and update function in the backend

--- api/src/controller/PostController.ts
+++ api/src/controller/PostController.ts
@@ -2,14 +2,60 @@ import { Request, Response } from "express";
 import { prisma } from "../utils/prisma";
 
 export class PostController {
-    async getUsers(_: Request, res: Response) {
+    async getExercises(_: Request, res: Response) {
         try {
 
-            const users = await prisma.user.findMany()
+            const exersices = await prisma.exercise.findMany()
 
-            return res.status(200).json({ users })
-        } catch (err) {
-            return res.status(400).json({ error: "Failed in fetching users" })
+            return res.status(200).json({ exersices })
+        } catch {
+            return res.status(500).json({ error: "Failed in fetching exersices" })
         }
     }
+
+    async addExercise(req: Request, res: Response) {
+        try {
+            const newExercise = await prisma.exercise.create({
+                data: {
+                    name: req.body.name,
+                    series: req.body.series,
+                    repetitions: req.body.repetitions,
+                    from: {
+                        connect: { id: req.body.wokoutId },
+                    },
+                }
+            })
+
+            return res.status(200).json({ newExercise })
+        } catch {
+            return res.status(500).json({ error: 'Failed in create exercise' })
+        }
+    }
+
+    async updateExersice(req: Request, res: Response) {
+
+        try {
+            const updateExersice = await prisma.exercise.update({
+                where: {
+                    id: req.body.id
+                },
+
+                data: {
+                    name: req.body.name,
+                    series: req.body.series,
+                    repetitions: req.body.repetitions,
+                    from: {
+                        connect: { id: req.body.wokoutId },
+                    },
+                },
+
+            })
+
+            return res.status(200).json({ updateExersice })
+        } catch {
+            return res.status(500).json({ error: 'Failed in update user' })
+        }
+    }
+
+
 }
